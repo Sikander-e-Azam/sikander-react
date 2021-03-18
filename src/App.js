fb0ca1197e9addb614179da4ce377ad7e0a5f4ad
Sikander <sazam.bscs15seecs@seecs.edu.pk>
2021-03-19 04:39:00 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {} from 'react-bootstrap '
+function App() {
+  return (
+    <div className="App">
+      <h1>Sikander React</h1>
+      
+    </div>
+  );
+}
+
+export default App;
